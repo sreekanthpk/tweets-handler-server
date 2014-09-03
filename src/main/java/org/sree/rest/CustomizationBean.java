8dc7f4ed092d892d2b507cf7d1e65c7b540c7f23
pk.sreekanth@gmail.com <pk.sreekanth@gmail.com>
2014-09-03 23:03:53 +0800

    Added SSL connector

---
+++ src/main/java/org/sree/rest/CustomizationBean.java
@@ -0,0 +1,35 @@
+package org.sree.rest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomizationBean {
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setPort(9000);
+		factory.setSessionTimeout(10, TimeUnit.MINUTES);
+		Connector httpsConnector = new Connector();
+		httpsConnector.setPort(9001);
+		httpsConnector.setSecure(true);
+		httpsConnector.setScheme("https");
+		httpsConnector.setAttribute("keyAlias", "tomcat");
+		httpsConnector.setAttribute("keystorePass", "password");
+		httpsConnector
+				.setAttribute(
+						"keystoreFile",
+						"C:/work/workspace/restful-server-standalone/src/main/resources/keystore/keystore.jks");
+		httpsConnector.setAttribute("clientAuth", "false");
+		httpsConnector.setAttribute("sslProtocol", "TLS");
+		httpsConnector.setAttribute("SSLEnabled", true);
+		factory.addAdditionalTomcatConnectors(httpsConnector);
+		return factory;
+	}
+}
